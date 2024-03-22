edb678f5aa4683c8cdcc5defbaab4a48571d7416
Cursedpongdevs <cursedpong@gmail.com>
2024-03-21 19:36:07 -0700

    Updating

---
+++ flagquiz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ~/Applications;
+echo "Downloading app...";
+curl https://the-okapi.github.io/sh/Flag-Quiz.zip -o fquiz.zip;
+echo "Unzipping app...";
+tar -xzf fquiz.zip;
+rm fquiz.zip;
+cd ~/Library/Application\ Support;
+mkdir flagquiz;
+cd flagquiz;
+echo "Downloading assets...";
+curl https://the-okapi.github.io/sh/flags.zip -o flags.zip;
+echo "Unzipping assets...";
+tar -xzf flags.zip;
+rm flags.zip;
